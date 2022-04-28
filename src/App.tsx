8fd37b8288a73a72debfe1b2dc1f0ba2e6a91441
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-28 18:17:47 -0300

    fix: update file reference

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import FrontPage from './components/FrontPage';
+import FrontPage from './pages/FrontPage';
 import './App.css';
 
 function App() {
