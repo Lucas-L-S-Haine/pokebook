5caed13e1e709393ee2cac29a0eeeafed0595441
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-28 16:35:15 -0300

    feat: create PokemonDetails component

---
+++ src/components/PokemonDetails.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export interface PokemonDetailsProps {
+}
+
+export interface PokemonDetailsState {
+}
+
+class PokemonDetails extends Component<PokemonDetailsProps, PokemonDetailsState> {
+}
+
+export default PokemonDetails;
