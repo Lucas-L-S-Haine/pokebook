0a71c1e271c713d6d01a4776114a63769bf86f1b
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-28 01:38:20 -0300

    refactor: get pokemon either by name or dex number

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,5 +1,5 @@
-export async function catchPokemon(speciesName: string) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${speciesName}`;
+export async function catchPokemon(pokemonId: number | string) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   const pokemon = await fetch(url)
     .then((binData) => binData.json())
     .then((jsonData) => jsonData);
