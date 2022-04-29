0be2067b265494b7ed4fcf3cca4524f21bacbf28
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-28 21:51:59 -0300

    feat: add some details

--- src/pages/PokemonDetails.tsx
+++ src/pages/PokemonDetails.tsx
@@ -1,12 +1,59 @@
 import { Component } from 'react';
+import { catchPokemonSpecies } from '../services/pokemon';
+import './PokemonDetails.css';
 
 export interface PokemonDetailsProps {
+  pokemonId: number | string;
 }
 
 export interface PokemonDetailsState {
+  pokemon: any;
+  loading: boolean;
 }
 
 class PokemonDetails extends Component<PokemonDetailsProps, PokemonDetailsState> {
+  constructor(props: PokemonDetailsProps) {
+    super(props);
+    this.state = {
+      pokemon: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { pokemonId } = this.props;
+    this.getPokemonSpecies(pokemonId);
+  }
+
+  async getPokemonSpecies(pokemonId: number | string) {
+    this.setState({ loading: true });
+    await catchPokemonSpecies(pokemonId)
+      .then((pokemon) => this.setState({ pokemon, loading: false }));
+  }
+
+  render() {
+    const { pokemon } = this.state;
+    return(
+      <div>
+        <div>
+          pokedex number
+          { pokemon.id }
+        </div>
+        <div>
+          name
+          { pokemon.name }
+        </div>
+        <div>
+          base happiness
+          { pokemon.base_happiness }
+        </div>
+        <div>
+          capture rate
+          { pokemon.capture_rate }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PokemonDetails;
