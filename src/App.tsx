0ea942eb88c63c193fb4986a93d12bb0ee2510f2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-28 21:15:42 -0300

    refactor: remove browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import FrontPage from './pages/FrontPage';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-pokemon">
-          <FrontPage />
-        </header>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-pokemon">
+        <FrontPage />
+      </header>
+    </div>
   );
 }
 
