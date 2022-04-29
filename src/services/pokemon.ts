fed89725c7bc58a4de7507fba2a2e665b49f201e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-04-28 21:39:07 -0300

    feat: add new method

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -5,3 +5,11 @@ export async function catchPokemon(pokemonId: number | string) {
     .then((jsonData) => jsonData);
   return pokemon;
 }
+
+export async function catchPokemonSpecies(pokemonId: number | string) {
+  const url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`;
+  const pokemon = await fetch(url)
+    .then((binData) => binData.json())
+    .then((jsonData) => jsonData);
+  return pokemon;
+}
